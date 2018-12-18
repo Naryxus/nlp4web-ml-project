9522eb47ff48f2e60058c0940efa873e5e78e748
Bene <Benedikt_Lins@gmx.de>
2018-12-18 15:05:54 +0100

    lala

--- src/main/java/features/PosFeature.java
+++ src/main/java/features/PosFeature.java
@@ -21,6 +21,8 @@ public class PosFeature extends FeatureExtractorResource_ImplBase implements Fea
 		POS tag = (POS) JCasUtil.selectCovering(view, POS.class, target.getBegin(), target.getEnd()).get(0);
 		Set<Feature> out = new HashSet<>();
 		
+		POS tmp = target.get;
+		
 		// I-ORG, I-PER
 		Feature posUH = new Feature("POS UH", false);
 		if (tag.getPosValue().equals("UH"))
@@ -30,7 +32,7 @@ public class PosFeature extends FeatureExtractorResource_ImplBase implements Fea
 		Feature posNNSYM = new Feature("POS NNSYM", false);
 		if (tag.getPosValue().equals("NN|SYM"))
 			posNNSYM.setValue(true);
-		out.add	(posNNSYM);
+		out.add(posNNSYM);
 		
 		Feature posNNPS = new Feature("POS NNPS", false);
 		if (tag.getPosValue().equals("NNPS"))
@@ -41,8 +43,6 @@ public class PosFeature extends FeatureExtractorResource_ImplBase implements Fea
 		if (tag.getPosValue().equals("NNP"))
 			posNNP.setValue(true);
 		out.add(posNNP);
-		//System.out.println(out.toString());
-		//System.out.println(tag.toString() + "\n\n");
 		return out;
 	}
 
